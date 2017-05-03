884e06a3f8c7c4e661ef9d8f28b35dd8338d2059
mizchi <miz404@gmail.com>
2017-05-04 07:15:06 +0900

    Update

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const yaml = require('js-yaml')
 const fs = require('fs')
-const rc = yaml.safeLoad(
-  fs.readFileSync(__dirname + '/eslintrc.yml')
-)
+const rc = yaml.safeLoad(fs.readFileSync(__dirname + '/eslintrc.yml'))
 module.exports = rc
